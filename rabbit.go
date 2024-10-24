978900b97bdb236aeda0a7623113166321e07004
arshia <arshiarezagholi1212@gmail.com>
2024-10-24 12:28:58 +0330

    completedf the Connect method

--- rabbit.go
+++ rabbit.go
@@ -1,6 +1,11 @@
 package rabbit_helper
 
-import "github.com/rabbitmq/amqp091-go"
+import (
+	"github.com/rabbitmq/amqp091-go"
+	"log"
+	"math"
+	"time"
+)
 
 type Rabbit struct {
 	RabbitUrl  string
@@ -8,6 +13,42 @@ type Rabbit struct {
 	Channel    *amqp091.Channel
 }
 
-func (rabbit *Rabbit) Connect(rabbitUrl string) (Rabbit, error) {
+func (rabbit *Rabbit) Connect(rabbitUrl string) error {
+	var connection *amqp091.Connection
+	var channel *amqp091.Channel
+	var counts int64
+	var backOff = 1 * time.Second
 
+	for {
+		c, err := amqp091.Dial(rabbitUrl)
+		if err != nil {
+			log.Println("rabbitmq not yet ready...!")
+			counts++
+		} else {
+			log.Println("connected to RabbitMQ")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			log.Println(err)
+			return err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Printf("backing off for %v seconds\n", backOff)
+		time.Sleep(backOff)
+
+	}
+
+	ch, err := connection.Channel()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	channel = ch
+
+	rabbit.Connection = connection
+	rabbit.Channel = channel
+	return nil
 }
